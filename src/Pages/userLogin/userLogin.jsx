5d6ece4688c91bf1bcfd56604e34816de68e3227
moochisillay-eng <moochisillay@gmail.com>
2026-05-26 09:31:47 -0300

    updagradews

--- src/Pages/userLogin/userLogin.jsx
+++ src/Pages/userLogin/userLogin.jsx
@@ -6,22 +6,31 @@ const UserLogin = () => {
     return (
         <article className={`${styles.contents} mainpanel`}>
             <div className={styles.innerContent}>
+                <div className={styles.overflowInnercontent}>
                 <section className={styles.separationSection}>
                     <h1 className={styles.msg}>{msg}!</h1>
                     <h3 className={styles.textDescription}>bem vindo ao jornal donaduzzi</h3>
+
                 </section>
                 <section className={styles.separationSection}>
-                    <h2 className={styles.title}>email</h2>
-                    <input className={styles.field}
-                        type="text"
-                        placeholder="---"
-                    />
-                     <h2 className={styles.title}>senha</h2>
-                    <input className={styles.field}
-                        type="password"
-                        placeholder="---"
-                    />
+                    <section className={styles.fieldcontainer}>
+                        <h2 className={styles.title}>insira seu email</h2>
+                        <input className={styles.field}
+                            type="text"
+                            placeholder="email"
+                        />
+                    </section>
+                    <section className={styles.fieldcontainer}>
+                        <h2 className={styles.title}>insira sua senha</h2>
+                        <input className={styles.field}
+                            type="password"
+                            placeholder="senha"
+                        />
+
+                    </section>
+                    <button className={styles.signinbutton}>criar minha conta</button>
                 </section>
+                </div>
             </div>
         </article>
     )
