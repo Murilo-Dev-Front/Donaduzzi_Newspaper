73a92fd30d527fd2aff8ff86c06355913b2b41d5
Murilo-Dev-Front <murilo.lang.alves@gmail.com>
2026-06-12 09:58:03 -0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 //pages
 import UserRegister from './Pages/userRegister/userRegister'
 import UserLogin from './Pages/userLogin/userLogin'
